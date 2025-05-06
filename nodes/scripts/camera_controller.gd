4824bae02360bdc015fa7e2c6b2ed5813adad2d1
Merchant151 <joshntram@gmail.com>
2025-05-05 21:20:21 -0400

    basic camera controls added

--- nodes/scripts/camera_controller.gd
+++ nodes/scripts/camera_controller.gd
@@ -1,1 +1,19 @@
-extends Node
+extends Node3D
+
+#export makes vars accessible in the editor 
+@export var speed = 50;
+
+#process should work like on update
+func _process(delta):
+	var velocity = Vector3.ZERO
+	if Input.is_action_pressed("right"):
+		velocity.x += 1 
+	if Input.is_action_pressed("left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("up"):
+		velocity.y += 1 
+	if Input.is_action_pressed("down"):
+		velocity.y -= 1
+	if velocity.length() > 0: 
+		velocity = velocity.normalized() * speed
+	position += velocity * delta
